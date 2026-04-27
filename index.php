82009213dea46f3b1590e75eae4e484144872b7a
Jonatas Souza <jonatas.cesouza@gmail.com>
2026-04-27 18:54:39 -0300

    Float Verification

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+$num1 = 10 - 5.2;
+$num2 = -153 + 27.94;
+$num3 = 89.99;
+
+if (is_float($num1)) {
+    echo "$num1 is a float.<br>";
+} else {
+    echo "$num1 is not a float.<br>";
+}
+
+
+if (is_float($num2)) {
+    echo "$num2 is a float.<br>";
+} else {
+    echo "$num2 is not a float.<br>";
+}
+
+if (is_float($num3)) {
+    echo "$num3 is a float.<br>";
+} else {
+    echo "$num3 is not a float.<br>";
+}
