c9500a5ab852966f8ce1596a8435504c33ada024
Jonatas Souza <jonatas.cesouza@gmail.com>
2026-04-27 20:51:06 -0300

    Using Array

--- index.php
+++ index.php
@@ -1,24 +1,13 @@
 <?php
 
-$num1 = 10 - 5.2;
-$num2 = -153 + 27.94;
-$num3 = 89.99;
 
-if (is_float($num1)) {
-    echo "$num1 is a float.<br>";
-} else {
-    echo "$num1 is not a float.<br>";
-}
-
-
-if (is_float($num2)) {
-    echo "$num2 is a float.<br>";
-} else {
-    echo "$num2 is not a float.<br>";
-}
-
-if (is_float($num3)) {
-    echo "$num3 is a float.<br>";
-} else {
-    echo "$num3 is not a float.<br>";
-}
+$car_caracteristics = [
+    "color" => "red",
+    "brand" => "Fiat",
+    "model" => "Uno",
+    "year" => 2010,
+    "fuel" => "gasoline",
+    "doors" => 4
+];
+echo "The color this car is " . $car_caracteristics["color"] . "<br>";
+echo "The car uses " . $car_caracteristics["fuel"];
