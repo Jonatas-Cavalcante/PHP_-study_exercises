fc1823890f55af9073caa7e8050425def2393f44
Jonatas Souza <jonatas.cesouza@gmail.com>
2026-04-27 23:34:32 -0300

    Exercise on Classes, Objects, this, Validation.

--- index.php
+++ index.php
@@ -1,28 +1,32 @@
 <?php
+class Estoque{
+    public $item;
+    public $quantidade;
 
-Class Equipamento{
-    public $nome;
-    public $tipo;
-    public $valor;
+    Function verificarestoque(){
+        $quantidade = $this->quantidade;
+        $item = $this->item;
 
-    function exibirDetalhes(){
-        echo "Nome; $this->nome <br>";
-        echo "Tipo; $this->tipo <br>";
-        echo "Valor; $this->valor <br>";
+        if($quantidade > 0)
+            {
+                return "O estoque de $item possui $quantidade unidades";
+            }
+            else{
+                return "ALERTA: Estoque de $item esgotado ou inválido!";
+            }
     }
 }
 
-$equipamento1 = new Equipamento();
-$equipamento1->nome = "Monitor DELL";
-$equipamento1->tipo = "Full HD 24 polegadas 160Hz";
-$equipamento1->valor = 1500.00;
 
-$equipamento2 = new Equipamento();
-$equipamento2->nome = "Teclado Mecânico";
-$equipamento2->tipo = "RGB Switch Red - ABNT2 - USB - Preto - Redragon";
-$equipamento2->valor = 300.00;
+$objeto1 = new Estoque();
+$objeto1->item = "Pinho";
+$objeto1->quantidade = 10;
 
-echo "Equipamento 1 <br>";
-$equipamento1->exibirDetalhes();
-echo "<br>Equipamento 2 <br>";
-$equipamento2->exibirDetalhes();
+$objeto2 = new Estoque();
+$objeto2->item = "Cedro";
+$objeto2->quantidade = -1;
+
+echo $objeto1->verificarestoque();
+echo "<br>";
+echo $objeto2->verificarestoque();
+?>
