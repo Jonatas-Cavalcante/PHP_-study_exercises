6788b789bb04b9dd9f1fc1b455835c84c0bb9178
Jonatas Souza <jonatas.cesouza@gmail.com>
2026-04-27 21:51:05 -0300

    Class and Object Exercise

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php
 
-$people_caracteristics = [
-    "color" => "white",
-    "size" => "big",
-    "years_old" => 30,
-    "hight" => 1.80,
-    "hair_color" => "black"
-];
-
-if ($people_caracteristics['years_old'] >= 18) {
-    echo "You are an adult";
-} else {
-    echo "You are a minor";
+class Pessoa{
+     public $nome;
+    
+    function falar(){
+        return "Olá mundo!";
+    }
 }
+
+$variavel1 = new Pessoa();
+
+$variavel1->nome = "João";
+
+echo $variavel1->nome;
+
+echo "<br>";
+echo $variavel1->falar();
