f2465fe4dfd31131505ec77124b1a125da024eef
Jônatas Cavalcante <jonatas.cesouza@gmail.com>
2026-04-29 21:02:08 -0300

    Using Static Scope in the Function

--- index.php
+++ index.php
@@ -1,32 +1,18 @@
 <?php
-class Estoque{
-    public $item;
-    public $quantidade;
 
-    Function verificarestoque(){
-        $quantidade = $this->quantidade;
-        $item = $this->item;
-
-        if($quantidade > 0)
-            {
-                return "O estoque de $item possui $quantidade unidades";
-            }
-            else{
-                return "ALERTA: Estoque de $item esgotado ou inválido!";
-            }
-    }
+function contador() {
+    static $contandor = 0;
+    $contandor++;
+    return "Contador: $contandor <br>";
 }
 
 
-$objeto1 = new Estoque();
-$objeto1->item = "Pinho";
-$objeto1->quantidade = 10;
-
-$objeto2 = new Estoque();
-$objeto2->item = "Cedro";
-$objeto2->quantidade = -1;
-
-echo $objeto1->verificarestoque();
-echo "<br>";
-echo $objeto2->verificarestoque();
-?>
+echo contador();
+echo contador();
+echo contador();
+echo contador();
+echo contador();
+echo contador();
+echo contador();
+echo contador();
+echo contador();
