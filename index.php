70cfa916a1edb54f87b6992b6377e72a7245346f
Jônatas Cavalcante <jonatas.cesouza@gmail.com>
2026-05-01 17:46:26 -0300

    usyng if and else and float

--- index.php
+++ index.php
@@ -1,30 +1,24 @@
 <?php
 
-$v1 = "Hello World";
-$v2 = 10;
-$v3 = true;
+$peso1 = 90.5;
+$peso2 = 69.9;
 
-if (is_int($v1)) {
-    echo "v1 is a integer\n";
-}
-else {
-    echo "v1 is not a integer\n";
-}
 
-echo "<br>";
-
-if (is_int($v2)) {
-    echo "v2 is a integer\n";
+if($peso1 <= 80){
+    echo "Peso dentro do Limite";
 }
-else {
-    echo "v2 is not a integer\n";
+else{
+    echo "Está pesado demais";
 }
 
 echo "<br>";
 
-if (is_int($v3)) {
-    echo "v3 is a interger\n";
+if($peso2 <= 80){
+    echo "Peso dentro do Limite";
 }
-else {
-    echo "v3 is not a integer\n";
+else{
+    echo "Está pesado demais";
 }
+
+
+?>
