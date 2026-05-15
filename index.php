8945bc77caaa410ab7f374309da40b14753b85c7
Jônatas Cavalcante <jonatas.cesouza@gmail.com>
2026-05-15 01:26:13 -0300

    Exercicio 32 Funcao

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 
-$lista = ["Maça", "Pera", "Uva", "Morango"];
+function Multiplicador(){
+    $num1 = 10;
+    $num2 = 15;
+    $num3 = 20;
 
+    $result = $num1 * $num2 * $num3;
+    echo "Resultado: " . $result;  
+}
 
-$contagem = implode(" , ", $lista);
-
-echo($contagem);
-
+Multiplicador();
